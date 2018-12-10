fac5fd190508f2b509f16e0be1c671ce1be454a9
Richard See <richard@Richards-MacBook-Pro.local>
2018-12-10 23:41:47 +0000

    Created constants file

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+// Unique authorisation key
+export const API_TOKEN = 'c3388f7729cb9c1364701c49434f6a4e860ec761874ec9d228042afb017a15ae'
+
+// LIFX API Endpoints
+export const GET_LIGHTS_URL = 'https://api.lifx.com/v1/lights/all'
+export const TOGGLE_URL = 'https://api.lifx.com/v1/lights/all/toggle'
+export const SET_STATE_URL = 'https://api.lifx.com/v1/lights/all/state'
+export const BREATHE_URL = 'https://api.lifx.com/v1/lights/all/effects/breathe'
