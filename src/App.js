9d9d8904788a3ade9aa3ad0cc9b031625f971427
Richard See <richard@Richards-MacBook-Pro.local>
2018-12-10 23:29:00 +0000

    Another hardcoded button to set state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import logo from './logo.svg';
 import './App.css';
 
 const TOGGLE_URL = 'https://api.lifx.com/v1/lights/all/toggle'
+const SET_STATE_URL = 'https://api.lifx.com/v1/lights/all/state'
 const API_TOKEN = 'c3388f7729cb9c1364701c49434f6a4e860ec761874ec9d228042afb017a15ae'
 const headers = {
-  'Authorization': `Bearer ${API_TOKEN}`
+  Authorization: `Bearer ${API_TOKEN}`
 }
 
 const toggleLight = () => {
   axios.post(TOGGLE_URL, null, { headers })
+}
+
+const turnLightRed = () => {
+  axios.put(
+    SET_STATE_URL,
+    {
+      power: 'on',
+      color: '#FF0000',
+      duration: 5,
+      brightness: 0.1
+    },
+    {
+      headers
+    }
+  )
 }
 
 class App extends Component {
@@ -31,7 +47,10 @@ class App extends Component {
             Learn React
           </a>
           <button onClick={toggleLight}>
-            Click me
+            Toggle Light
+          </button>
+          <button onClick={turnLightRed}>
+            Turn light red
           </button>
         </header>
       </div>
