8fbbb12183ac6ae1fc6dd4c971a799cb45820234
Richard See <richard@Richards-MacBook-Pro.local>
2018-12-10 23:43:47 +0000

    Added HTTP type

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@
 export const API_TOKEN = 'c3388f7729cb9c1364701c49434f6a4e860ec761874ec9d228042afb017a15ae'
 
 // LIFX API Endpoints
-export const GET_LIGHTS_URL = 'https://api.lifx.com/v1/lights/all'
-export const TOGGLE_URL = 'https://api.lifx.com/v1/lights/all/toggle'
-export const SET_STATE_URL = 'https://api.lifx.com/v1/lights/all/state'
-export const BREATHE_URL = 'https://api.lifx.com/v1/lights/all/effects/breathe'
+export const GET_LIGHTS_URL = 'https://api.lifx.com/v1/lights/all' // GET
+export const TOGGLE_URL = 'https://api.lifx.com/v1/lights/all/toggle' // POST
+export const SET_STATE_URL = 'https://api.lifx.com/v1/lights/all/state' // PUT
+export const BREATHE_URL = 'https://api.lifx.com/v1/lights/all/effects/breathe' // POST
