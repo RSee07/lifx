c981fdaf1d3cc80a3280a460352fa2bbe9544655
Richard See <richard@Richards-MacBook-Pro.local>
2018-12-16 16:26:34 +0000

    Updates

---
+++ precache-manifest.33729e6ad19c09eb394f6d50c196fbac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3d432fcd4e820a21ff4",
+    "url": "/lifx/static/js/main.b3d432fc.chunk.js"
+  },
+  {
+    "revision": "871ba64fdedcd55325c5",
+    "url": "/lifx/static/js/1.871ba64f.chunk.js"
+  },
+  {
+    "revision": "aabb73a04e98b177a2b5",
+    "url": "/lifx/static/js/runtime~main.aabb73a0.js"
+  },
+  {
+    "revision": "cbec358711ee151d68a1f18723bc748d",
+    "url": "/lifx/index.html"
+  }
+];
